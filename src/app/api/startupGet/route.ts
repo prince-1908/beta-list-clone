8ff217205b7d03f0396d1f5b1d752b89f0a5d061
Prince Singh Chouhan <princesinghchouhan7470@gmail.com>
2023-10-19 02:36:54 +0530

    force-dynamic

--- src/app/api/startupGet/route.ts
+++ src/app/api/startupGet/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+export const dynamic = 'force-dynamic';
 export async function GET(){
 
     const getData = await prisma.startups.findMany();
